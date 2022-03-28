7392a79def7dc416b7c694c68cd3a623f9ce7ff5
MD Ashik Ali <mdashika989@yahoo.com>
2022-03-28 15:40:27 +0600

    load latest notice and events for home page

--- index.js
+++ index.js
@@ -369,6 +369,17 @@ const run = async () => {
       res.json(result)
     });
 
+    // load home and letest event
+    app.get("events/home", async (req, res) => {
+      const result = await eventsCollection.find({}).limit(3).sort({_id:-1}).toArray();
+      res.json(result);
+    })
+    // load home and letest notice
+    app.get("notice/home", async (req, res) => {
+      const result = await noticeCollection.find({}).limit(3).sort({_id:-1}).toArray();
+      res.json(result);
+    })
+
 
     // update verification
     app.put("/verify/:id", async (req, res) => {
